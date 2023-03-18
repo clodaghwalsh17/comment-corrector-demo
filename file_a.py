77d6adbdc49718e5c282f22f5c0be514c752a3fa
Clodagh Walsh <clodaghwalsh17@gmail.com>
2023-03-18 20:20:14 +0000

    Add Python files for testing
    Signed-off-by: Clodagh Walsh <clodaghwalsh17@gmail.com>

---
+++ file_a.py
@@ -0,0 +1,6 @@
+cost = 100
+# Set the variable tax to 0.5
+tax = 0.5
+price = cost
+# Print value of tax
+print(tax)
---
+++ file_b.py
@@ -0,0 +1,7 @@
+# Some other comment that refers to tax
+cost = 100
+# Set the variable tax to 0.5
+my_num = 0.5
+price = cost
+# Print value of tax
+print(my_num)
