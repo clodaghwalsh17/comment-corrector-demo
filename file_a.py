a48cff93e974e7513e93200409a9f3c8b0987b14
Clodagh Walsh <42538837+clodaghwalsh17@users.noreply.github.com>
2023-03-24 22:02:04 +0000

    Add comments

--- file_a.py
+++ file_a.py
@@ -1,3 +1,6 @@
+# File authored by clodagh
+# TODO finish the code
+# Some other comment that refers to tax, recognoise that tax is outdated variable
 cost = 100
 # Set the variable tax to 0.5
 tax = 0.5
