743adfe79f31661fb20de153a93bb4f2516ef666
Clodagh Walsh <42538837+clodaghwalsh17@users.noreply.github.com>
2023-03-27 16:39:51 +0100

    Create file_d.py

---
+++ file_d.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: iso-8859-15 -*-
+
+def string_manipulation(str):
+    # Display original strg
+    print(str)
+    str = str.capitalize() # Capitalise first letter of string
+    print(str)
