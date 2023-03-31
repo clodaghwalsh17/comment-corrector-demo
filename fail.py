643850c57b8f9f2b4b1d06ef4a42f076fdee90d2
Clodagh Walsh <42538837+clodaghwalsh17@users.noreply.github.com>
2023-03-31 13:31:11 +0100

    Create fail.py

---
+++ fail.py
@@ -0,0 +1,19 @@
+"""
+Add two components, x and y do not have to be of the same type
+param x: First parameter
+param y: Second parameter
+return: Return addition of x and y 'accordingly'
+"""
+
+def add(x,y):
+    return x + y
+
+'''
+Add two components, x and y do not have to be of the same type
+param x: First parameter
+param y: Second parameter
+return: Return addition of x and y 'accordingly'
+'''
+
+def subtract(x,y):
+    return x + y
