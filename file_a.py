68dac8bd008308815c0e78e24eac1d95247f2608
Clodagh Walsh <42538837+clodaghwalsh17@users.noreply.github.com>
2023-03-31 11:16:38 +0100

    Update file_a.py

--- file_a.py
+++ file_a.py
@@ -3,7 +3,6 @@
 # Some other comment that refers to tax, recognoise that tax is outdated variable
 cost = 100
 # Set the variable tax to 0.5
-tax = 0.5
+tax = 0.6
 price = cost
-# Print value of tax
-print(tax)
+# return tax
