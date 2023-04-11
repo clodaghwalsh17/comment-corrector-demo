e017a7e0373c27ba7df68b9fe6ec37f86a9936df
Clodagh Walsh <clodaghwalsh17@gmail.com>
2023-04-11 23:48:11 +0100

    Update verison 2 of files
    Signed-off-by: Clodagh Walsh <clodaghwalsh17@gmail.com>

--- circle.py
+++ circle.py
@@ -1,10 +1,10 @@
-import random # Built in module
+import random
 
 PI = 3.14 # Value of pi
 
 # Find area of circle with random radius
 def area():   
-   r = random.randint(1, 5) # Random value between 1 and 5
+   r = random.randint(1, 6) # Random value between 1 and 5
    area = PI * r * r # Formula for area for circle
    # Print information
    print("Radius={}, Area={}".format(r,area))
--- tax.py
+++ tax.py
@@ -1,8 +1,7 @@
 # File authored by clodagh
 
 # TODO finish the function
-def calculate_tax(income):
-    tax_rate = 0.5
+def calculate_tax(income, tax_rate):
     tax_payable = income * tax_rate
     print(tax_payable)
     # return tax_payable
