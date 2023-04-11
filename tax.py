86dad2928c7fa723a497a295ff1d014b1d7c17a5
Clodagh Walsh <clodaghwalsh17@gmail.com>
2023-04-12 00:07:13 +0100

    At bug task comment
    Signed-off-by: Clodagh Walsh <clodaghwalsh17@gmail.com>

--- tax.py
+++ tax.py
@@ -1,7 +1,9 @@
 # File authored by clodagh
 
 # TODO finish the function
-def calculate_tax(income, tax_rate):
+def calculate_tax(income, tax_rate=0.5):
     tax_payable = income * tax_rate
     print(tax_payable)
-    # return tax_payable
+    # BUG
+    # if income < 0:
+    #   print("Not taxable")
